536d96adcb42d2e68980d96887294c17717f9e8b
Nimira43 <n.rabone72@aol.com>
2025-07-10 18:55:33 +0100

    Creating driverData array of objects. First driver done - Verstappen

---
+++ js/driver-data.js
@@ -0,0 +1,22 @@
+export const driverData = [
+  {
+    firstname: 'Max',
+    lastname: 'Verstappen',
+    team: 'Red Bull',
+    nationality: 'Dutch',
+    dateOfBirth: '30/09/1997',
+    grandPrixStarts: 221,
+    wins: 65,
+    poles: 44,
+    fastestLaps: 34,
+    podiums: 117,
+    points: 3188.5,
+    worldChampionships: [
+      2021, 2022, 2023, 2024
+    ],
+    coreSkill: 99,
+    wetWeatherMod: 0.099,
+    tyreManagementMod: 0.099,
+    reliabilityMod: 0.099
+  }
+]
