c82c077feff09e0d4ae2555500b16eb2483a773d
Nimira43 <n.rabone72@aol.com>
2025-07-10 22:40:48 +0100

    Created utilites - holding points data. Will hold any other smaller datasets

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 import { driverData } from './js/driver-data.js'
+import { calendarData } from './js/calendar-data.js'
+
+import { grandPrixPoints, sprintRacePoints, fastestLapPoint } from './js/utilities.js'
 
 console.log(driverData)
+console.log(calendarData)
+console.log(grandPrixPoints)
+console.log(sprintRacePoints)
+console.log(fastestLapPoint)
