f4f4d03451794e95048282452169fb6f6e58163c
Nimira43 <n.rabone72@aol.com>
2025-07-11 02:46:13 +0100

    Trying the formula out for McLaren at Round 12

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import { driverData } from './js/driver-data.js'
 import { calendarData } from './js/calendar-data.js'
 import { teamData } from './js/team-data.js'
+import { financialPerformanceEngine } from './js/finance/seasonFinancials.js'
 import { grandPrixPoints, sprintRacePoints, fastestLapPoint, tyreCompounds } from './js/utilities.js'
 
 console.log(driverData)
@@ -10,3 +11,29 @@ console.log(grandPrixPoints)
 console.log(sprintRacePoints)
 console.log(fastestLapPoint)
 console.log(tyreCompounds)
+
+
+console.log(financialPerformanceEngine(teamData[0], 12))
+
+/*
+{teamName: 'McLaren', performanceScore: 87, penaltyTriggered: false, remainingBudget: 7}
+penaltyTriggered : false
+performanceScore : 87
+remainingBudget : 7
+teamName : "McLaren"
+
+Performance Score: 87
+This means McLaren are still operating near their peak efficiency despite mid-season fatigue.
+
+Their high budgetEfficiency (91) and solid techUpgradeRate (89) are keeping performance smooth.
+
+The formula factors in how well they're converting money into race-day output — and McLaren’s doing it well.
+
+Remaining Budget: $7M
+With only $7M left from their $165M cap, they’ve spent 96% — aggressive but not reckless.
+
+This suggests most major upgrades or developments are already in play.
+
+You can simulate tight strategy choices from here — maybe one final upgrade, or resource-saving tactics in later rounds.
+
+*/
