2c50faa1da3c8a930e81e37bd0b75597c1ea2fcd
Nimira43 <n.rabone72@aol.com>
2025-08-12 07:38:43 +0100

    Classes

--- simulator/script.js
+++ simulator/script.js
@@ -1,2 +1,18 @@
 import { drivers } from './driversData.js'
 
+class InputHandler {
+  constructor() {}
+}
+class RoadLine {
+  constructor() {}
+}
+class Player {
+  constructor() {}
+}
+class Enemy {
+  constructor() {}
+}
+class Game {
+  constructor() {}
+}
+new Game()
