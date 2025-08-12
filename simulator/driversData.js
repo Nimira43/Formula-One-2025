3924cb3b995e847e710cfd1bf6d347d73979bbb0
Nimira43 <n.rabone72@aol.com>
2025-08-12 07:25:35 +0100

    drivers data array of objects - Red Bull

---
+++ simulator/driversData.js
@@ -0,0 +1,16 @@
+export const drivers = [
+  {
+    driver: 'Verstappen',
+    team: 'Red Bull',
+    colour: '#00008b',
+    car: 33,
+    badge: '#ffff00'
+  },
+  {
+    driver: 'Tsunoda',
+    team: 'Red Bull',
+    colour: '#00008b',
+    car: 22,
+    badge: '#ffff00'
+  },
+]
