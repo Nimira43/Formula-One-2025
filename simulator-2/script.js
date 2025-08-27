4416ef3e5b30b83c960dc35ccf2201c1a051808b
Nimira43 <n.rabone72@aol.com>
2025-08-27 01:26:02 +0100

    Added AI car - kind of works!

--- simulator-2/script.js
+++ simulator-2/script.js
@@ -12,6 +12,24 @@ const canvas = document.getElementById('track')
     width: 20, height: 40
   }
 
+  const aiCar = {
+    x: 100,
+    y: 100,
+    angle: 0,
+    speed: 2,
+    width: 20,
+    height: 40,
+    pathIndex: 0
+  } 
+
+  const aiPath = [
+    { x: 100, y: 100 },
+    { x: 700, y: 100 },
+    { x: 700, y: 500 },
+    { x: 100, y: 500 }
+  ] 
+
+
   const keys = {}
   document.addEventListener('keydown', e => keys[e.key] = true)
   document.addEventListener('keyup', e => keys[e.key] = false)
@@ -29,8 +47,23 @@ const canvas = document.getElementById('track')
     car.y -= Math.cos(car.angle) * car.speed
   }
 
+  function updateAICar() {
+    const target = aiPath[aiCar.pathIndex] 
+    const dx = target.x - aiCar.x 
+    const dy = target.y - aiCar.y 
+    const distance = Math.sqrt(dx * dx + dy * dy) 
+
+    if (distance < 5) {
+      aiCar.pathIndex = (aiCar.pathIndex + 1) % aiPath.length 
+    } else {
+      aiCar.angle = Math.atan2(dy, dx) 
+      aiCar.x += Math.cos(aiCar.angle) * aiCar.speed 
+      aiCar.y += Math.sin(aiCar.angle) * aiCar.speed 
+    }
+  }
+
   function drawCar() {
-    ctx.save();
+    ctx.save() 
     ctx.translate(car.x, car.y)
     ctx.rotate(car.angle)
     ctx.fillStyle = 'red'
@@ -38,20 +71,31 @@ const canvas = document.getElementById('track')
     ctx.restore()
   }
 
+  function drawAICar() {
+    ctx.save() 
+    ctx.translate(aiCar.x, aiCar.y) 
+    ctx.rotate(aiCar.angle) 
+    ctx.fillStyle = 'blue' 
+    ctx.fillRect(-aiCar.width/2, -aiCar.height/2, aiCar.width, aiCar.height) 
+    ctx.restore() 
+  }
+
   function drawTrack() {
-    ctx.fillStyle = '#333';
-    ctx.fillRect(50, 50, 700, 500);
-    ctx.strokeStyle = '#fff';
-    ctx.lineWidth = 4;
-    ctx.strokeRect(50, 50, 700, 500);
+    ctx.fillStyle = '#333' 
+    ctx.fillRect(50, 50, 700, 500) 
+    ctx.strokeStyle = '#fff' 
+    ctx.lineWidth = 4 
+    ctx.strokeRect(50, 50, 700, 500) 
   }
 
   function loop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    drawTrack()
-    update()
-    drawCar()
-    requestAnimationFrame(loop)
+    ctx.clearRect(0, 0, canvas.width, canvas.height) 
+    drawTrack() 
+    update() 
+    updateAICar() 
+    drawCar() 
+    drawAICar() 
+    requestAnimationFrame(loop) 
   }
 
   loop()
